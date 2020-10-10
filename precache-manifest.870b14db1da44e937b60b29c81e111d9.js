da4921483f6bccc79d1e9d8cc43c0ff2002eb510
yogesh <yt4049952@gmail.com>
2020-10-10 20:39:03 +0530

    Updates

---
+++ precache-manifest.870b14db1da44e937b60b29c81e111d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6a209b628b1481ccd9662aec48a3dd1",
+    "url": "/demo-react-gh-pages/index.html"
+  },
+  {
+    "revision": "1741eb34309521aa9185",
+    "url": "/demo-react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4e5aa1e41374b58b4fec",
+    "url": "/demo-react-gh-pages/static/js/2.7b022445.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo-react-gh-pages/static/js/2.7b022445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1741eb34309521aa9185",
+    "url": "/demo-react-gh-pages/static/js/main.1d7a314b.chunk.js"
+  },
+  {
+    "revision": "862dbeed5213c22c550d",
+    "url": "/demo-react-gh-pages/static/js/runtime-main.651fc32d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
